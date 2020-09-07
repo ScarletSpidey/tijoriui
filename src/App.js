0ea4f804954231f1cc8a556ad7e6665c51e9c76e
Devansh Swarup <42760954+devansh2712@users.noreply.github.com>
2020-09-07 20:41:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,59 @@
+//import React, {Component} from "react";
+//import logo from "./bundle.png";
+//import "./App.css";
+//import Button from 'react-bootstrap/Button';
+//import Navbar from 'react-bootstrap/Navbar'
+//import Nav from 'react-bootstrap/Nav'
+//import NavDropdown from 'react-bootstrap/NavDropdown'
+//import Form from 'react-bootstrap/Form'
+//import FormControl from 'react-bootstrap/FormControl'
+//import Card from 'react-bootstrap/Card'
+
+
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Navigation, Footer, Home, About, DAO, Projects, ProjectProfile, DaoCards, ProjectPage, ReactPage} from "./components";
+import AboutPage from "./components/AboutPage"
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navigation />
+        <Switch>
+        
+          <Route path="/" exact component={() => <Home />} />
+          <Route path="/Project" exact component={() => <Projects />} />
+          <Route path="/DAO" exact component={() => <DAO />} />
+
+          <Route 
+            exact
+            path="/DAO/:id"
+            component={ReactPage}
+          />
+
+          <Route 
+            exact
+            path="/Project/:id"
+            component={ReactPage}
+          />
+
+          <Route path="/about" exact component ={() => <AboutPage /> } />
+
+          <Route path="/ProjectPage" exact component={() => <ProjectPage />} />
+          <Route path="/ReactPage" exact component={() => <ReactPage />} />
+
+
+        </Switch>
+        
+      </Router>
+    </div>
+  );
+}
+
+
+
+
+
+
+export default App;

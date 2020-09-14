e04f3aa4f48bb0ec8b2e665451964e385ba99888
ap-aryanpandey <aryanappandey@gmail.com>
2020-09-14 20:10:21 +0530

    Revert "Update App.js"
    This reverts commit 86f7eac408a16c3e0f8b2308845e0b8b9b4fe0c5.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@
 
 
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Navigation, Footer, Home, About, DAO, Projects, ProjectProfile, DaoCards, ProjectPage, ReactPage} from "./components";
 import AboutPage from "./components/AboutPage"
 
